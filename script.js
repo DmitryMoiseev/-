4875f5bda1ebd41858b5995a156e71e4fa8e87a7
DmitryMoiseev <43666821+DmitryMoiseev@users.noreply.github.com>
2018-12-21 09:42:57 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/* переменные для счета*/
+let myScore = 0;
+let pcScore = 0;
+
+/*Выделение вокруг элементов*/
+let img = document.querySelectorAll(".pink");
+for(let i = 0; i < img.length; i++){          //при наведении
+    img[i].onmouseover = function(event){ 
+    let target = event.target; 
+    target.style.backgroundColor = 'pink'; 
+    } 
+    } 
+    
+    for(let i = 0; i < img.length; i++){      //в стоке
+    img[i].onmouseout = function(event){ 
+    let target = event.target; 
+    target.style.backgroundColor = 'transparent'; 
+    } 
+    }
+
+
+ $('.ssp li').each(function(){
+    $(this).on('click', function(){
+        $('.result li:eq(0)').first().find('p').text($(this).find('p').text());
+        switch (Math.floor(Math.random()*3)){
+            case 0: $('.result li').last().find('p').text('камень');break;
+            case 1: $('.result li').last().find('p').text('ножницы');break;
+            case 2: $('.result li').last().find('p').text('бумага');break;
+        }
+        
+        if($('.result li').first().find('p').text() == $('.result li').last().find('p').text()){ 
+             $('.total').last().find('p').text('ничья'); 
+           
+            } else if ($('.result li').first().find('p').text() == 'бумага' && $('.result li').last().first().find('p').text() == 'камень' || $('.result li').first().find('p').text() == 'камень' && $('.result li').last().find('p').text() == 'ножницы' || $('.result li').first().find('p').text() == 'ножницы' && $('.result li').last().find('p').text() == 'бумага'){
+            
+            $('.total').last().find('p').text('вы выиграли C:'); 
+            
+            $('.My').text(++myScore); 
+        } else {
+            $('.total').last().find('p').text('вы проиграли :C'); 
+            $('.PC').text(++pcScore); 
+        }
+    });
+});
