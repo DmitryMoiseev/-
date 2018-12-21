e965f019761a6bc81fda15da1e62c1840ae45981
DmitryMoiseev <43666821+DmitryMoiseev@users.noreply.github.com>
2018-12-21 09:52:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,23 +11,24 @@ for(let i = 0; i < img.length; i++){          //при наведении
     } 
     } 
     
-    for(let i = 0; i < img.length; i++){      //в стоке
+    for(let i = 0; i < img.length; i++){      //без наведения
     img[i].onmouseout = function(event){ 
     let target = event.target; 
     target.style.backgroundColor = 'transparent'; 
     } 
     }
 
-
+/*вывод выбора игрока */
  $('.ssp li').each(function(){
     $(this).on('click', function(){
         $('.result li:eq(0)').first().find('p').text($(this).find('p').text());
+        /*вывод выбора компьютера */
         switch (Math.floor(Math.random()*3)){
             case 0: $('.result li').last().find('p').text('камень');break;
             case 1: $('.result li').last().find('p').text('ножницы');break;
             case 2: $('.result li').last().find('p').text('бумага');break;
         }
-        
+        /*сравнение результатов и определение победителя */
         if($('.result li').first().find('p').text() == $('.result li').last().find('p').text()){ 
              $('.total').last().find('p').text('ничья'); 
            
